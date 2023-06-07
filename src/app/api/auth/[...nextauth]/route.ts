8a48510b59e5d05a628d14ac4c826d80ecfb93e3
Adrian Kristoffer Borgund <Adrian.Kristoffer.Borgund@dnb.no>
2023-06-07 13:05:43 +0200

    Remove export on route beauce it broke the build. Added Auth0 Provider.

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,17 +1,31 @@
-import NextAuth from "next-auth";
+import NextAuth, { NextAuthOptions } from "next-auth";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import type { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import Auth0Provider from "next-auth/providers/auth0";
 import { prisma } from "@/lib/prisma";
 
-export const authOptions: NextAuthOptions = {
-  //adapter: PrismaAdapter(prisma),
+const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
     }),
+    Auth0Provider({
+      clientId: process.env.AUTH0_CLIENT_ID!,
+      clientSecret: process.env.AUTH0_CLIENT_SECRET!,
+      issuer: process.env.AUTH0_ISSUER!,
+    }),
   ],
+  theme: {
+    colorScheme: "dark",
+  },
+  callbacks: {
+    async jwt({ token }) {
+      token.userRole = "user";
+      return token;
+    },
+  },
 };
 
 const handler = NextAuth(authOptions);
